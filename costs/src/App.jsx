93e042117ef3d16ce27a70e9884535ed52b6896f
BrunoSilva03 <bruno5ssilva02@gmail.com>
2023-09-06 14:14:50 -0300

    Update App.jsx

--- costs/src/App.jsx
+++ costs/src/App.jsx
@@ -1,13 +1,30 @@
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
 
 
   return (
     <>
-     <div className="App">
-      <p>Costs</p>
-     </div>
+     <Router>
+      <ul>
+        <li>Home</li>
+        <li>Contato</li>
+      </ul>
+      <Routes>
+        <Route exact path="/">
+            <Home />
+        </Route>
+        <Route exact path="/company">
+            <Company/>
+        </Route>
+        <Route exact path="/contact">
+          <Contact/>
+        </Route>
+        <Route exact path="/newproject">
+          <NewProject />
+        </Route>
+      </Routes>
+     </Router>
     </>
   )
 }
