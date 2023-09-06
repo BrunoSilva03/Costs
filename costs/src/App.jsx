6b6b3f4407dca158428716a47bd3be5b1d001dff
BrunoSilva03 <bruno5ssilva02@gmail.com>
2023-09-06 15:46:50 -0300

    criando os menus de navegação

--- costs/src/App.jsx
+++ costs/src/App.jsx
@@ -1,31 +1,44 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Home from './components/pages/Home'
+import Company from './components/pages/Company'
+import Contact from './components/pages/Contact'
+import NewProject from './components/pages/NewProject'
+
+import Container from './components/layoult/Container'
+import styles from './components/layoult/Container.module.css'
 
 function App() {
 
 
   return (
-    <>
-     <Router>
-      <ul>
-        <li>Home</li>
-        <li>Contato</li>
-      </ul>
-      <Routes>
-        <Route exact path="/">
-            <Home />
-        </Route>
-        <Route exact path="/company">
-            <Company/>
-        </Route>
-        <Route exact path="/contact">
-          <Contact/>
-        </Route>
-        <Route exact path="/newproject">
-          <NewProject />
-        </Route>
-      </Routes>
-     </Router>
-    </>
+
+    <Router>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contato</Link>
+        <Link to="/company">Empresa</Link>
+        <Link to="/newproject">Novo Projeto</Link>
+      </div>
+      <Container>
+
+        <Routes>
+
+          <Route exact path="/" element={<Home />}></Route>
+
+          <Route exact path="/company" element={<Company />}></Route>
+
+          <Route exact path="/contact" element={<Contact />}></Route>
+
+          <Route exact path="/newproject" element={<NewProject />}></Route>
+
+
+        </Routes>
+
+      </Container>
+
+
+    </Router>
+
   )
 }
 
