de9ed66a9327681ac6a06f7190ec84a1a13be023
BrunoSilva03 <bruno5ssilva02@gmail.com>
2023-09-07 14:13:36 -0300

    ajeitando e tornando bonito o NavBar

--- costs/src/components/layoult/NavBar.jsx
+++ costs/src/components/layoult/NavBar.jsx
@@ -3,19 +3,30 @@ import { Link } from 'react-router-dom'
 import Container from './Container'
 
 import styles from './NavBar.module.css'
-import logo from '../../img/costs_logo.png'
+import logo from '../../imagens/costs_logo.png'
 
 function NavBar() {
     return (
-        <nav>
+        <nav class={styles.navbar}>
             <Container>
                 <Link to="/">
                     <img src={logo} alt="Costs" />
                 </Link>
-                <Link to="/">Home</Link>
-                <Link to="/contact">Contato</Link>
-                <Link to="/company">Empresa</Link>
-                <Link to="/newproject">Novo Projeto</Link>
+                <ul class={styles.list}>
+                    <li class={styles.item}>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li class={styles.item}>
+                        <Link to="/contact">Contato</Link>
+                    </li>
+                    <li class={styles.item}>
+                        <Link to="/company">Empresa</Link>
+                    </li>
+                    <li class={styles.item}>
+                        <Link to="/newproject">Novo Projeto</Link>
+                    </li>
+                </ul>
+
             </Container>
         </nav>
     )
