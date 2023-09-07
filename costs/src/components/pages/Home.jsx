074bf73e381c8676b512b1e461ea84c25034e33c
BrunoSilva03 <bruno5ssilva02@gmail.com>
2023-09-07 15:42:46 -0300

    Finalizando a Home

--- costs/src/components/pages/Home.jsx
+++ costs/src/components/pages/Home.jsx
@@ -1,6 +1,7 @@
 import styles from './Home.module.css'
 
 import savings from '../../imagens/savings.svg'
+import LinkButton from '../layoult/LinkButton'
 
 function Home() {
     return (
@@ -9,7 +10,7 @@ function Home() {
                 Bem-vindo ao <span>Costs</span>
             </h1>
             <p>Comece a gerenciar os seus projetos agora mesmo!</p>
-            <a href="/">Criar Projeto</a>
+            <LinkButton to="/newproject" text="Criar Projeto" />
             <img src={savings} alt="Costs" />
         </section>
     )
