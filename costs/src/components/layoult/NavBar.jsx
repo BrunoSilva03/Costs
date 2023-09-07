68af63610d6c1fbbc77e1eefb0d0110a8ff82bf7
BrunoSilva03 <bruno5ssilva02@gmail.com>
2023-09-07 14:45:45 -0300

    finalizando footer

--- costs/src/components/layoult/NavBar.jsx
+++ costs/src/components/layoult/NavBar.jsx
@@ -7,25 +7,25 @@ import logo from '../../imagens/costs_logo.png'
 
 function NavBar() {
     return (
-        <nav class={styles.navbar}>
+        <nav className={styles.navbar}>
             <Container>
                 <Link to="/">
                     <img src={logo} alt="Costs" />
                 </Link>
-                <ul class={styles.list}>
-                    <li class={styles.item}>
+                <ul className={styles.list}>
+                    <li className={styles.item}>
                         <Link to="/">Home</Link>
                     </li>
-                    <li class={styles.item}>
+                    <li className={styles.item}>
                             <Link to="/projects">Meus Projetos</Link>
                     </li>
-                    <li class={styles.item}>
+                    <li className={styles.item}>
                         <Link to="/company">Empresa</Link>
                     </li>
-                    <li class={styles.item}>
+                    <li className={styles.item}>
                         <Link to="/newproject">Novo Projeto</Link>
                     </li>
-                    <li class={styles.item}>
+                    <li className={styles.item}>
                         <Link to="/contact">Contato</Link>
                     </li>
                 </ul>
