932eadd79dbce9da4a2bd909aa2633435a550766
BrunoSilva03 <bruno5ssilva02@gmail.com>
2023-09-12 09:33:56 -0300

    useEffect no Projects

--- costs/src/components/pages/Projects.jsx
+++ costs/src/components/pages/Projects.jsx
@@ -1,17 +1,31 @@
 import { useLocation } from 'react-router-dom'
 
+import { useState, useEffect } from 'react'
+
 import Message from '../layoult/Message'
 import Container from '../layoult/Container'
 import LinkButton from '../layoult/LinkButton'
+import ProjectCard from '../project/ProjectCard'
 
 import styles from './Projects.module.css'
 
 function Projects() {
+    const [projects, setProjects] = useState([])
+
     const location = useLocation()
     let message = ''
     if (location.state) {
         message = location.state.message
     }
+
+    useEffect(() => {
+        fetch('http://localhost:5000/projects', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+    }, [])
 
 
 return (
