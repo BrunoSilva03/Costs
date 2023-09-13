59e1bf2716a030c4e0a58266f4a9eaf778e11143
BrunoSilva03 <bruno5ssilva02@gmail.com>
2023-09-13 14:26:57 -0300

    fazendo o update

--- costs/src/components/pages/Project.jsx
+++ costs/src/components/pages/Project.jsx
@@ -3,13 +3,18 @@ import styles from './Project.module.css'
 import { useParams } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 
+
+import Loading from '../layoult/Loading'
+import Container from '../layoult/Container'
+
 function Project() {
     const { id } = useParams()
 
     const [project, setProject] = useState([])
 
     useEffect(() => {
-        fetch(`http://localhost:5000/projects/${id}`, {
+        setTimeout(() => {
+            fetch(`http://localhost:5000/projects/${id}`, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
@@ -20,10 +25,25 @@ function Project() {
             setProject(data)
           })
           .catch((err) => console.log)
+        }, 333)
     }, [id])
     
     return (
-        <p>{project.name}</p>
+        <>
+        {project.name ? (
+            <div>
+                <Container customClass="column">
+                    <div>
+                        <h1>Projeto: {project.name}</h1>
+                        <button>Editar projeto</button>
+                    </div>
+                </Container>
+            </div>
+            
+        ) : (
+            <Loading />
+        )}
+        </>
     )
 }
 
